c2a2dfada780e577d5e3a4a118ee8c35b749e925
Kristina Zhukovskaya <70661760+cristmasbend@users.noreply.github.com>
2020-11-18 13:42:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-left"><img src="images/leftArrow.svg" alt="left"></button>',
+        prevArrow: '<button class="slick-arrow slick-right"><img src="images/rightArrow.svg" alt="right"></button>',
+    fade: true
+    });
+});
+
+
+
+
